9a69999a2ff037ec859f447b2369c2b102f96a13
unknown <agutleben@outlook.fr>
2020-12-16 16:40:51 +0100

    presque fin

--- mainV2.py
+++ mainV2.py
@@ -9,22 +9,44 @@ links_cat = []
 links_books = []
 list_cat = []
 book_info = []
+links_other_page = []
+links_all_page = []
 
 
 # Recuperer toutes les catégories
 def get_categories():
     cats = soup.find(class_='nav nav-list')
-    for i in cats:
-        ul = cats.find('ul')
-        li = ul.findAll('li')
+
+    ul = cats.find('ul')
+    li = ul.findAll('li')
 
     for i in li:
         a = i.find('a')
         link = a['href']
-        categorie = a.text
 
+        categorie = a.text.replace(" ", "").strip('\n')
         links_cat.append('http://books.toscrape.com/' + link)
         list_cat.append(categorie)
+
+
+def get_number_page():
+    num = 0
+    ind = 1
+
+    for i in list_cat:
+        url1 = links_cat[num]
+
+        num += 1
+        ind += 1
+        res = requests.get(url1)
+        sp = BeautifulSoup(res.text, features="html.parser")
+        page = sp.find(class_='current')
+        if page:
+            nbre_page = page.text.replace(" ", "")
+            max_page = nbre_page[-3]
+            for j in range(int(max_page)-1):
+                url2 = 'http://books.toscrape.com/catalogue/category/books/' + i.replace(" ", "").lower().strip('\n') + "_" + str(ind) + "/page-" + str(j + 2) + ".html"
+                links_other_page.append(url2)
 
 
 def get_all_book_by_categorie():
@@ -32,84 +54,54 @@ def get_all_book_by_categorie():
     max = 1
 
     for i in list_cat:
+        links_all_page.clear()
         url1 = links_cat[num]
+        links_all_page.append(url1)
+
+        matching = filter(lambda x: i.lower() in x, links_other_page)
+
+        for k in matching:
+            links_all_page.append(k)
+
         num += 1
-        res = requests.get(url1)
-        sp = BeautifulSoup(res.text, features="html.parser")
-        page = sp.find(class_='current')
-        result = sp.find(class_='form-horizontal')
 
-        if result:
-            nbre_str = result.text.replace(" ", "")
-            nbre_result = nbre_str[3:5]
+        for link in links_all_page:
+            res = requests.get(link)
+            sp = BeautifulSoup(res.text, features="html.parser")
+            page = sp.find(class_='current')
+            result = sp.find(class_='form-horizontal')
 
-        if page:
-            nbre_page = page.text.replace(" ", "")
-            max_page = nbre_page[-3]
-            max_page = max
-            ints = int(nbre_result) / (int(max_page) * 20)
+            books = sp.findAll(class_="image_container")
 
-        books = sp.findAll(class_="image_container")
-        count = 0
-        for u in books:
+            #Création url du livre
+            for u in books:
+                a = u.find('a')
+                link = a['href']
+                link_str = link.split("/")
 
-            a = u.find('a')
-            link = a['href']
-            link_str = link.split("/")
-
-            if not page:
-                print("no enter")
                 links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/" + link_str[4])
                 link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + link_str[4]
 
-            if page:
-                print(count)
-                print("page")
-
-                if 140 > count <= 160:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-7.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-7.html"
-                    count += 1
-                if 120 > count <= 140:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-6.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-6.html"
-                    count += 1
-                if 100 > count <= 120:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-5.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-5.html"
-                    count += 1
-                if 100 > count <= 120:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-5.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-5.html"
-                    count += 1
-                if 100 > count <= 120:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-5.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-5.html"
-                    count += 1
-                if 80 > count <= 100:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-4.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-4.html"
-                    count += 1
-                    print(count)
-                if 40 > count <= 60:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-3.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-3.html"
-                    count += 1
-                if count > 20:
-                    print("ok")
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/page-2.html")
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + "/page-2.html"
-
-                    count += 1
-                if count <= 20:
-                    links_books.append('http://books.toscrape.com/catalogue/' + link_str[3] + "/" + link_str[4])
-                    link_final = 'http://books.toscrape.com/catalogue/' + link_str[3] + "/" + link_str[4]
-                    count += 1
-                    print("moins")
+                get_book_info(link_final)
 
 
-            get_book_info(link_final)
+        extract_to_csv(book_info,i+".csv")
 
+
+
+def extract_to_csv(liste, output_file, delimiter="\t"):
+    header = ["universal_ product_code (upc)","title","price_including_tax", "price_excluding_tax",
+              "number_available","product_description","category","review_rating", "image_url"]
+    with open(output_file, "w") as output_file:
+
+        for i in header:
+
+            output_file.write(i + '\t')
+
+        output_file.write('\n')
+        for i in liste:
+
+            output_file.write(str(i) + '\t')
 
 def get_book_info(url_book):
     res = requests.get(url_book)
@@ -121,6 +113,7 @@ def get_book_info(url_book):
 
 
 get_categories()
+get_number_page()
 get_all_book_by_categorie()
 
-[print(i) for i in book_info]
+
